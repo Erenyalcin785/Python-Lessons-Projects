baaab6e41c722575fec34b9e060b155426755f6d
Erenyalcin785 <ernylcn0101@gmail.com>
2026-01-13 23:24:19 +0300

    Update 65_Matplotlib.py

--- EREN Python/65_Matplotlib.py
+++ EREN Python/65_Matplotlib.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt 
 
 
-
 #seri1 = [1,2,3,4,5]
 #seri2 = [6,7,8,9,10]
 #plt.plot(seri1,seri2) # ilk değer x ikinci değer y kordinatını verir
 #plt.show()
-
 
 Yıl = [2015,2016,2017,2018,2019,2020,2021]
 fiyat = [100,200,300,400,500,600,700]
@@ -16,4 +14,5 @@ plt.title("2015-2021 FİYAT")
 plt.xlabel("Yıl")
 plt.ylabel("Fiyat")
 plt.show()
+
 
